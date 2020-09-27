d60a77957035d21076c142e3b31e8a40adf52cb1
Morgan Gardner <morgan.gardner2112@gmail.com>
2020-09-27 20:02:50 +0100

    added pin declarations

--- rf24n_sim_receiver/rf24n_sim_receiver.ino
+++ rf24n_sim_receiver/rf24n_sim_receiver.ino
@@ -3,25 +3,34 @@
 #include <RF24Network.h>
 #include "printf.h"
 
-RF24 radio(8,10);                  // nRF24L01(+) radio attached using Getting Started board 
+//NRF Setup
+const int CE_pin = 8;   //CE Pin Assignment
+const int CS_pin = 10;  //CSN Pin Assignment
+const uint16_t this_node = 00;    // Address of receiver
+const uint16_t other_node = 01;   // Address of sender
 
-RF24Network network(radio);       // Network uses that radio
-const uint16_t this_node = 00;    // Address of our node in Octal format ( 04,031, etc)
-const uint16_t other_node = 01;   // Address of the other node in Octal format
+//LED Setup
+const int LED_pin = 3;
+const int NUM_LEDS = 8;
 
-//LED setup
-#define NUM_LEDS 8
+RF24 radio(CE_pin,CS_pin);                  // nRF24L01(+) radio
+
+//Radio init
+RF24Network network(radio);
+
+
+//LED init
 CRGB leds[NUM_LEDS];
 
 
 void setup(void)
 {
-  FastLED.addLeds<NEOPIXEL, 3>(leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, LED_pin>(leds, NUM_LEDS);
   FastLED.clear();
   FastLED.show();
   
   radio.begin();
-  network.begin(/*channel*/ 90, /*node address*/ this_node);
+  network.begin(90, this_node);
 }
 
 void loop(void){
@@ -31,10 +40,32 @@ void loop(void){
   while ( network.available() ) {     // Is there anything ready for us?
     
     RF24NetworkHeader header;        // If so, grab it and print it out
-    byte ledData[24];
+    network.peek(header);
+
+    switch (header.type)
+    {
+      case 'l':   //LED packet
+      UpdateLED(header);
+      break;
+      
+      case 'n':   //Nextion packet
+      UpdateNextion(header);
+      break;
+
+      default:    //Unknown packet
+
+      break;
+    }
+
+  }
+}
+
+void UpdateLED(RF24NetworkHeader& header)
+{
+    byte ledData[NUM_LEDS * 3];
     network.read(header,&ledData,sizeof(ledData));
 
-    for (int i = 0; i <= 7; i++)  //per LED
+    for (int i = 0; i <= NUM_LEDS - 1; i++)  //per LED
     {
       leds[i].r = ledData[(i*3)];
       leds[i].g = ledData[(i*3)+1];
@@ -42,5 +73,9 @@ void loop(void){
     }
 
     FastLED.show();
-  }
+}
+
+void UpdateNextion(RF24NetworkHeader& header)
+{
+
 }
